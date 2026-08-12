09a9efe65e35d4a3401db33640bf95753ae3af0c
Sushant Kumar <sushant843120@gmail.com>
2026-08-13 00:57:00 +0530

    feat: strengthen recruiter-first portfolio contact data

--- src/data/site.ts
+++ src/data/site.ts
@@ -2,6 +2,8 @@ export const siteConfig = {
   name: 'Sushant Kumar',
   title: 'Java Backend Developer',
   email: 'sushant843120@gmail.com',
+  phone: '+91 9608379492',
+  phoneHref: 'tel:+919608379492',
   linkedin: 'https://www.linkedin.com/in/sushant-kumar-05a5342b4/',
   github: 'https://github.com/sushantapi',
   siteUrl: 'https://www.sushantkumar.co.in',
